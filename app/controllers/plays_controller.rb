cfe5b77bf17a84803a4dae6ec1c9d4cf1f2ae7d1
htri <inamovload@yahoo.com>
2016-03-26 19:52:08 +0700

    Add Average Review and User authenticate

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,5 +1,6 @@
 class PlaysController < ApplicationController
 	before_action :find_play, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_user!, only: [:edit, :new]
 	def index
 		if params[:category].blank?
 			@plays = Play.all.order("created_at DESC")
@@ -15,6 +16,11 @@ class PlaysController < ApplicationController
 	end
 
 	def show
+		if @play.reviews.blank?
+			@average_review = 0
+		else
+			@average_review = @play.reviews.average(:rating).round(2)
+		end
 	end
 
 	def create
